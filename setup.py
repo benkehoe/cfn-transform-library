3a85fecf11c901b4bc121f06dae1c201cb95a624
Unknown <bkehoe@irobot.com>
2018-01-26 08:28:53 -0500

    Rename, add requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='cfn-transform-lib',
+    name='cfn-transform-library',
     version='0.1.0',
     description='CloudFormation template transformations',
-    packages=["cfn_transform_lib"],
+    packages=["cfn_transform_library"],
+    install_requires=["cfn-transform"],
     author='Ben Kehoe',
     author_email='bkehoe@irobot.com',
     project_urls={
-        "https://github.com/benkehoe/cfn-transform-lib",
+        "https://github.com/benkehoe/cfn-transform-library",
     },
     license='Apache Software License 2.0',
     classifiers=(
